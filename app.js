0be096a53421df6336990dfae471fee28bcbe827
Mahima <mahimasachdeva223@gmail.com>
2019-01-14 21:29:56 +0530

    created first endpoint(to get todos)

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const app = express();
+const app = express();  //set up express app
+const db = require('./db/db.js');
+
+//get all todos
+app.get('/todos', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        todos: db
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
